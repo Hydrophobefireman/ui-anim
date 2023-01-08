4de0c0e69cca052af4064cdf800d97a877ecef5f
Hydrophobefireman <bhavesh.pareek36@gmail.com>
2023-01-08 15:32:07 +0530

    anim2

--- scripts/prebuild.js
+++
@@ -1,6 +0,0 @@
-const { join } = require("path");
-const { rmSync } = require("fs");
-const dist = join(__dirname, "..", "dist");
-console.log(`$ rm -rf ${dist}`);
-
-rmSync(dist, { recursive: true, force: true });
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-export { AnimateLayout } from "./AnimateLayout";
-export { Motion } from "./Motion";
-export { animate, interpolate } from "./util/animate";
-export { createSnapshot } from "./util/snapshot";
-export { DeclarativeTransform } from "./util/declarative-transform";
+export {AnimateLayout} from "./AnimateLayout";
+export {Motion} from "./Motion";
+export {animate, interpolate} from "./util/animate";
+export {createSnapshot} from "./util/snapshot";
+export {DeclarativeTransform} from "./util/declarative-transform";
 export * from "./types";
